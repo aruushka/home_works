9e2936922cb127b03c1de422c7954dbc0aba6a0f
aruushka <aruukeshamshievaa055@gmail.com>
2025-03-08 20:40:32 +0600

    Бул бутакта 07.03.25 уй тапшырмасы сакталды

--- home_work.py
+++ home_work.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+name = "aruuke"
+age = 18
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a = 34
+b = 43
+print(f'My name is {name.capitalize()}. I am {age} y.o. {a} + {b} = {a + b}')
